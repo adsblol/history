9c45e948fb4241e58c29ede41aa896f22b68ca76
k <k@example.com>
2023-02-01 01:02:50 +0100

    Remove minutes subfolder for history.adsb.lol JSONs

--- app.py
+++ app.py
@@ -9,8 +9,10 @@ from aiohttp import web
 import aiofiles
 from pigz_python import PigzFile
 from datetime import datetime
+
 routes = web.RouteTableDef()
 from settings import HUB, PORT
+
 
 async def fetch_remote_data(app):
     # This function will update the app["aircrafts"] list and store the last 30 copies of the aircrafts.json file
@@ -25,20 +27,25 @@ async def fetch_remote_data(app):
                     aircrafts = []
                     try:
                         async with session.get(f"http://{ip}/aircraft.json") as resp:
-                                aircrafts = await resp.json()
+                            aircrafts = await resp.json()
 
-                                # Store the last 30 copies of the aircrafts.json file
-                                # If it's the same as the last one, don't store it
-                                if len(app["aircrafts"]) > 0 and app["aircrafts"][-1] == aircrafts:
-                                    print("Same aircrafts.json, not storing")
-                                    continue
-                                if len(app["aircrafts"]) > 30:
-                                    app["aircrafts"].pop(0)
-                                    print("aircrafts.json list is too long, removing the oldest one")
-                                app["aircrafts"].append(aircrafts)
-                                print("aircrafts.json stored, len=", len(app["aircrafts"]))
+                            # Store the last 30 copies of the aircrafts.json file
+                            # If it's the same as the last one, don't store it
+                            if (
+                                len(app["aircrafts"]) > 0
+                                and app["aircrafts"][-1] == aircrafts
+                            ):
+                                print("Same aircrafts.json, not storing")
+                                continue
+                            if len(app["aircrafts"]) > 30:
+                                app["aircrafts"].pop(0)
+                                print(
+                                    "aircrafts.json list is too long, removing the oldest one"
+                                )
+                            app["aircrafts"].append(aircrafts)
+                            print("aircrafts.json stored, len=", len(app["aircrafts"]))
 
-                                await store_data(app)
+                            await store_data(app)
                     except Exception as e:
                         print(f"Error fetching aircrafts.json from {ip}: {e}")
                         traceback.print_exc()
@@ -46,6 +53,7 @@ async def fetch_remote_data(app):
                 await asyncio.sleep(1)
     except asyncio.CancelledError:
         print("Background task cancelled")
+
 
 async def store_data(app):
     # This function will look at app["aircrafts"] and pick,
@@ -60,43 +68,49 @@ async def store_data(app):
             for aircraft in app["aircrafts"][::-1]:
                 now = float(aircraft["now"])
                 tiers = {
-                    'gold': [5.5, 4.5],
-                    'silver': [7, 5.5],
-                    'bronze': [10, 7],
-                    'wood': [15, 10],
-                    'plastic': [20, 15],
-                    'paper': [30, 20],
-                    'stone': [60, 30],
+                    "gold": [5.5, 4.5],
+                    "silver": [7, 5.5],
+                    "bronze": [10, 7],
+                    "wood": [15, 10],
+                    "plastic": [20, 15],
+                    "paper": [30, 20],
+                    "stone": [60, 30],
                 }
                 # Ensure if it does not exist yet, it will be stored
                 if not app["last_stored_at"]:
                     candidate = aircraft
-                    print('last_stored_at does not exist yet')
+                    print("last_stored_at does not exist yet")
                     break
                 for tier, (max_age, min_age) in tiers.items():
-                    if now - app["last_stored_at"] > min_age and now - app["last_stored_at"] < max_age:
+                    if (
+                        now - app["last_stored_at"] > min_age
+                        and now - app["last_stored_at"] < max_age
+                    ):
                         candidate = aircraft
-                        print(f'tier={tier} now={now} last_stored_at={app["last_stored_at"]}')
+                        print(
+                            f'tier={tier} now={now} last_stored_at={app["last_stored_at"]}'
+                        )
                         break
             if not candidate:
-                print('No candidate found')
+                print("No candidate found")
                 return
             # Store the candidate in /out/YYYY/MM/DD/HH/MM/adsblol-YYYY-MM-DD-HH-MM-SS.json (pigz)
             candidate_datetime = datetime.fromtimestamp(candidate["now"])
-            candidate_datetime_str = candidate_datetime.strftime("%Y/%m/%d/%H/%M/adsblol-%Y-%m-%d-%H-%M-%S.json")
+            candidate_datetime_str = candidate_datetime.strftime(
+                "%Y/%m/%d/%H/adsblol-%Y-%m-%d-%H-%M-%S.json"
+            )
             candidate_path = f"/out/{candidate_datetime_str}"
             # make directory and subdirectories
             os.makedirs(os.path.dirname(candidate_path), exist_ok=True)
-            async with aiofiles.open(candidate_path, mode='wb') as f:
-                await f.write(json.dumps(candidate).encode('utf-8'))
+            async with aiofiles.open(candidate_path, mode="wb") as f:
+                await f.write(json.dumps(candidate).encode("utf-8"))
             # Update app["last_stored_at"]
             app["last_stored_at"] = candidate["now"]
-            print(f'stored={candidate_path}')
+            print(f"stored={candidate_path}")
 
             await asyncio.sleep(1)
     except asyncio.CancelledError:
         print("Background task cancelled")
-
 
 
 async def background_task_1(app):
@@ -106,6 +120,7 @@ async def background_task_1(app):
 
     app["fetch_remote_data"].cancel()
     await app["fetch_remote_data"]
+
 
 # aiohttp server
 app = web.Application()
@@ -117,5 +132,5 @@ app["last_stored_at"] = 0
 app.cleanup_ctx.append(background_task_1)
 
 if __name__ == "__main__":
-    aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader('/app/templates'))
+    aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader("/app/templates"))
     web.run_app(app, host="0.0.0.0", port=PORT)
